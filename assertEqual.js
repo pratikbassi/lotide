1bef780722b5267a295a53323233b09b16800c08
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-02-03 21:44:33 +0000

    Created the assertEqual.js function

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log('🍏🍏🍏Assertion Passed: "' + actual + '" === "' + expected + '"');
+  } else {
+    console.log('🍎🍎🍎Assertion Failed: "' + actual + '" !== "' + expected + '"');
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(2, 1);
+assertEqual("hello", "hello");
