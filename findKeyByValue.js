afb0c3be81d1ae6611b6c0d9d646eb7451b87245
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-02-04 22:52:11 +0000

    created findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`🍏🍏🍏Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🍎🍎🍎Assertion Failed: "${actual}" === "${expected}"`);
+  }
+};
+
+const findKeyByValue = function (inputObj, inputString) {
+  for(const item in inputObj){
+    if(inputObj[item] === inputString){
+      return item;
+    }
+  }
+  
+
+
+};
+
+
+
+
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
