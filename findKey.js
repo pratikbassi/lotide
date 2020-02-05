3ffbd474497250dad2d69dc2f97b221685693a9b
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-02-05 21:23:25 +0000

    finished findKey.js

---
+++ findKey.js
@@ -0,0 +1,23 @@
+const findKey = function (object, callBack) {
+  for(let item in object){
+    if(callBack(object[item])){
+      return item;
+    }
+  }
+}
+
+/// TESTING BELOW
+
+/*** 
+result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+console.log(result);
+
+***/
