647679e7699f7a584018022267595e26b6da165d
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-02-10 20:43:04 +0000

    made assertArraysEqual modular

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,29 +1,6 @@
-const eqArrays = function (array1, array2) {
-  let recursiveFlag = true;
-  if(array1.length !== array2.length){
-    //console.log('LENGTHS DID NOT MATCH');
-    recursiveFlag = false;
-  }
 
-  for(let i = 0; i < array1.length; i++){
-    if(Array.isArray(array1[i]) && Array.isArray(array2[i])){
-      //console.log('RECURSION REACHED');
-      recursiveFlag = eqArrays(array1[i], array2[i]);
-    }
-    else if ((!Array.isArray(array1[i]) && Array.isArray(array2[i])) || (Array.isArray(array1[i]) && !Array.isArray(array2[i]))){
-      //console.log('TYPES DID NOT MATCH');
-      recursiveFlag = false;
-    } else {
-      if(array1[i] !== array2[i]){
-        //console.log('ITEMS DID NOT MATCH');
-        recursiveFlag = false;
-      }
-    }
-  }
-  
 
-  return recursiveFlag;
-}
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function (value1, value2) {
 
@@ -34,4 +11,4 @@ const assertArraysEqual = function (value1, value2) {
   }
 };
 
-
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,4 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([0],[0]);
+assertArraysEqual([0,1,2],[0,1,2,3]);
